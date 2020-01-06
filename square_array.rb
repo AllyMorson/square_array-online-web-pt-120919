8c7c1eed1f7e52f46763ae449185e3ceb60d70c6
Ally Morson <ally.morson@gmail.com>
2020-01-06 17:26:56 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
 def square_array(numbers)
   squared_numbers = []
-  numbers = [1,2,3]
-  numbers.each do |num **2|
-    new_numbers << num 
-  end
-end 
+  numbers.each do |num|squared_numbers << num **2
+  squared_numbers
+end
